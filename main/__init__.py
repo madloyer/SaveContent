1f080b2004097d8bca761cadc2f4aa5f1e4d0517
madloyer <87596107+madloyer@users.noreply.github.com>
2023-08-19 15:21:26 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("23308278", default=None, cast=int)
+API_HASH = config("b9e478d024ad099bdeede4ef1b55a9b4", default=None)
+BOT_TOKEN = config("6659822191:AAEmq3h_mPqcX7x0Sfa01xBwN7o1p0gcmNQ", default=None)
+SESSION = config("AQC-HpEjear8g-zbnm-n4ZWwHry9_vmjBbKvJNk_BuK6BV4_JvoPVAvfiF8jghe-oCDOvoqNxmHWkVX3mOnkrkX2dIxtbeVYayxTPmFIFb5Jk3qgS_x3fjboDttstDfEVkHO_6cw286HChJB0F-zHAdigOHZ_qrQeL9kF5z5Mnlt1pQco2_0E40yxPVRu1Sc6x9L9qgYnksDOZtdWnVhQBpPPHSyC0Iff9yx4OjaAwvGrXW7b3qalAu9RqCCb9mvYOxgLnDhWg7m_bXuT1FRSAsh3EnJ3vXJKgv46u0HvN4v3gxltM7JAVbsfBJ51oGqnm6fMb9Xt5OwZaZhEYrTVjPXS2mvPAA", default=None)
+FORCESUB = config("mantapvids", default=None)
+AUTH = config("1281619082", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
